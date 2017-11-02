4769e0be1663c8b1bfc145f7544d3587048e1a54
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-02 19:45:46 +0000

    username field can receive input

--- src/chatbar.jsx
+++ src/chatbar.jsx
@@ -1,23 +1,55 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
 
 class ChatBar extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: "",
+      content: ""
+    };
   }
 
-  handleEnter = (e) => {
+  onContentEnter = (e) => {
     if (e.key === 'Enter') {
-    this.props.getMessage(e.target.value);
+      this.props.getMessage(this.state.username, this.state.content);
     }
+  }
+
+  onContentChange = (e) => {
+    this.setState({ content: e.target.value });
+  }
+
+  onUsernameEnter = (e) => {
+    if (e.key === 'Enter') {
+      this.props.onNewUsername(this.state.username);
+    }
+  }
+
+  onUsernameChange = (e) => {
+    this.setState({ username: e.target.value });
   }
 
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" type="text" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" type="text" placeholder="Type a message and hit ENTER" onKeyPress={ this.handleEnter } />
+
+        <input
+          className="chatbar-username"
+          type="text"
+          placeholder="Your Name (Optional)"
+          onChange={ this.onUsernameChange }
+          value={ this.state.username }
+          onKeyPress={ this.onUsernameEnter } />
+
+        <input
+          className="chatbar-message"
+          type="text"
+          placeholder="Type a message and hit ENTER"
+          onChange={ this.onContentChange }
+          value={ this.state.content }
+          onKeyPress={ this.onContentEnter } />
       </footer>
     );
   }
