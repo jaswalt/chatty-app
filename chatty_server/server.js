64e6cdd01ee6d0789c14edead69a680def72df2a
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-02 17:22:56 +0000

    debugs readyState error by renaming socketServer to webSocket and sends message to all clients

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws')
+const SocketServer = WebSocket.Server;
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,11 +14,24 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      console.log("Send to Client")
+      client.send(data);
+    }
+  });
+};
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
+  ws.on('message', (message) => {
+    wss.broadcast(message);
+    console.log(message);
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
