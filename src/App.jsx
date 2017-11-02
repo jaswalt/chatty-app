be0e84394599e4eaa92b68ad4cd7c9adbc84c319
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-02 02:12:34 +0000

    handles users typing new messages

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ class App extends Component {
       currentUser: {name: "Bob"},
       messages: [
         {
+          id: 1,
           username: "Bob",
           content: "Has anyone seen my marbles?",
         },
         {
+          id: 2,
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         }
@@ -21,13 +23,35 @@ class App extends Component {
     };
   }
 
+  addMessage = (message) => {
+    let newMessage = {
+      id: new Date().getUTCMilliseconds(),
+      username: "Anonymous",
+      content: message
+    };
+    this.setState({ messages: this.state.messages.concat(newMessage) })
+  }
+
   render() {
     return (
       <div>
-        <h1>This Is A Title</h1>
-        <ChatBar currentUser={this.state.currentUser} />
-        <MessageList messages={this.state.messages} />
+        <ChatBar currentUser={ this.state.currentUser } getMessage={ this.addMessage } />
+        <MessageList messages={ this.state.messages } />
       </div>);
   }
+
+  componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  }, 3000);
+}
+
 }
 export default App;
