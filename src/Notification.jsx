69d40abb878b3059292b4e57d973b877db7c4a45
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-03 02:58:25 +0000

    some tidying up

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -3,18 +3,11 @@ import React, {Component} from 'react';
 class Notification extends Component {
   render() {
     return (
-        <div className="message">
-          <span className="message-username">{ this.props.oldUsername }</span>
-            changed their name to
-          <span className="message-content">{ this.props.newUsername }</span>
-        </div>
-    )
-
+      <div className="message system">
+        { this.props.oldUsername } changed their name to { this.props.newUsername }
+      </div>
+    );
   }
-
 }
-
-
-
 
 export default Notification;
